686cfa3b6798f202b135e4afa2d28efc86e60266
Bohdanza <bohdanza87@gmail.com>
2023-08-17 15:00:37 +0300

    jumping time

--- goltseditor/hero.cs
+++ goltseditor/hero.cs
@@ -19,7 +19,7 @@ namespace goltseditor
         [JsonProperty]
         private long timeSinceJump = 0;//actual time in GAME TICKS since last jump started
         [JsonProperty]
-        private int maxJumpingTime = 12; //maximum time after leaving the land during which a jump can be performed
+        private int maxJumpingTime = 4; //maximum time after leaving the land during which a jump can be performed
 
         [JsonConstructor]
         public Hero() { }
@@ -27,7 +27,7 @@ namespace goltseditor
         public Hero(ContentManager contentManager, double x, double y, double movementX, double movementY)
             : base(contentManager, x, y, movementX, movementY, 5, true, "hero", @"boxes\hero", 5, 5, "id")
         {
-            StandartFallingSpeed = 0.6;
+            StandartFallingSpeed = 1;
         }
 
         public override void Update(ContentManager contentManager, World world)
